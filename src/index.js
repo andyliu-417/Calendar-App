00d499befacc958f3020625a7e4ca6e26ef67d22
jjunxi <andylucky417@gmail.com>
2018-05-07 21:49:30 -0400

    ant design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.css';
 import App from './components/App/App';
 import registerServiceWorker from './helpers/registerServiceWorker';
 
