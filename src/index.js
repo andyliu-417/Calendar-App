f13ace272c6ff95e1749a43e61bef50fc3a47f4a
jjunxi <andylucky417@gmail.com>
2018-05-07 21:52:03 -0400

    LocaleProvider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import 'antd/dist/antd.css';
 import App from './components/App/App';
 import registerServiceWorker from './helpers/registerServiceWorker';
+import enUS from 'antd/lib/locale-provider/en_US';
+import {LocaleProvider} from 'antd';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <LocaleProvider locale={enUS}>
+    <App/>
+  </LocaleProvider>
+), document.getElementById('root'));
 registerServiceWorker();
