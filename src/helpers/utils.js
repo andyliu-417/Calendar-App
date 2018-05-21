6048ddbda32a6fba1c5e8a0c31527f18300cb131
andyliu-417 <andy2018.liu@gmail.com>
2018-05-21 12:47:20 -0400

    toggle sort

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,6 +2,14 @@ import moment from "moment";
 
 export function compare() {
   return function(e1, e2) {
+    if (e1.completed && e2.completed) 
+    {
+      var m1 = moment(e1.datetime);
+      var m2 = moment(e2.datetime);
+      return m1.isAfter(m2);
+    }
+    if (e1.completed) return true;
+    if (e2.completed) return false;
     var m1 = moment(e1.datetime);
     var m2 = moment(e2.datetime);
     return m1.isAfter(m2);
